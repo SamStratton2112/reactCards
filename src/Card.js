d247113ccfe621be2bb90e805e86de484ac8e31d
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2023-12-30 22:13:09 -0800

    push part 1

---
+++ src/Card.js
@@ -0,0 +1,8 @@
+const Card = ({src='https://deckofcardsapi.com/static/img/back.png'}) =>{
+	return(
+    // use image url as src that was passed down as props. 
+		<img  src={src}/>
+  )
+}
+
+export default Card;
---
+++ src/Deck.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import Card from "./Card";
+
+const Deck = () => {
+  // initial value of null will be replace on render due to useEffect
+  const [deckId, setDeckId] = useState(null);
+  // initialize deck with back of card image 
+  const [currentCardImg, setCurrentCardImg] = useState('https://deckofcardsapi.com/static/img/back.png');
+
+  // async function to draw a card from same deck
+  const draw = async () => {
+    try{
+    // if there is a deck id then draw from said deck
+      if (deckId) {
+        const response = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
+        setCurrentCardImg(response.data.cards[0].image);
+      }
+    }catch{
+      // once all cards have been drawn alert and show card back 
+      alert('Error: no cards remaining!')
+      setCurrentCardImg('https://deckofcardsapi.com/static/img/back.png')
+    };
+  };
+
+  useEffect(() => {
+    // replace initial deckId value of null with a new deck 
+    axios.get('https://deckofcardsapi.com/api/deck/new/draw/?count=1')
+      .then(res => {
+        setDeckId(res.data.deck_id);
+      });
+  }, []);
+
+  // on click draw from deck until cards are gone
+  useEffect(() => {
+    draw();
+  }, [deckId]);
+
+  const handleClick = () => {
+    draw();
+  };
+
+  return (
+    <>
+      <Card src={currentCardImg}/>
+      <br></br>
+      <button onClick={handleClick}>Draw</button>
+    </>
+  );
+}
+
+export default Deck;
